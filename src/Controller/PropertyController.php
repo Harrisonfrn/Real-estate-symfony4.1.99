1f261e3096c66b4357dae634135facd3a59b21b7
HarrisonPro <frnharrison@gmail.com>
2020-02-05 16:52:09 +0100

    Getting property from database and adding it to home

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,35 +3,33 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController
 {
+
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
     /**
      * @Route("/biens", name="property.index")
      * @return Response
      */
     public function index(): Response
     {
-        $property = new Property;
-        $property->setTitle('My first home')
-            ->setPrice(200000)
-            ->setRooms(4)
-            ->setBedrooms(3)
-            ->setDescription('beautiful home')
-            ->setSurface(60)
-            ->setFloor(4)
-            ->setHeat(1)
-            ->setCity('Nancy')
-            ->setAddress('14 rue Notre Dame')
-            ->setZipCode('54000');
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($property);
-        $em->flush();
-
-        return $this->render("property/index.html.twig", [
+          return $this->render("property/index.html.twig", [
             'current_menu' => 'properties'
         ]);
     }

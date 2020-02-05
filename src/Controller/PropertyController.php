2c1239a69bb8ff0381f33e0cc4e0e13cb1ccb609
HarrisonPro <frnharrison@gmail.com>
2020-02-05 12:21:17 +0100

    Adding debug bar

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +15,22 @@ class PropertyController extends AbstractController
      */
     public function index(): Response
     {
+        $property = new Property;
+        $property->setTitle('My first home')
+            ->setPrice(200000)
+            ->setRooms(4)
+            ->setBedrooms(3)
+            ->setDescription('beautiful home')
+            ->setSurface(60)
+            ->setFloor(4)
+            ->setHeat(1)
+            ->setCity('Nancy')
+            ->setAddress('14 rue Notre Dame')
+            ->setZipCode('54000');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($property);
+        $em->flush();
+
         return $this->render("property/index.html.twig", [
             'current_menu' => 'properties'
         ]);
--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -9,6 +10,12 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Property
 {
+
+    const HEAT = [
+        0 => ' electric',
+        1 => 'gaz'
+    ]; 
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -74,12 +81,17 @@ class Property
     /**
      * @ORM\Column(type="boolean", options={"default": false})
      */
-    private $sold;
+    private $sold = false;
 
     /**
      * @ORM\Column(type="datetime")
      */
     private $created_at;
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTime();
+    }
 
     public function getId(): ?int
     {
